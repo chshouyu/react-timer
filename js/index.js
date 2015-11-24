81474dfba041d03c9f2d6878e4004df096d5b591
chshouyu <chshouyu@126.com>
2015-11-24 17:27:29 +0800

    change normalize.css path

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Timer from './Timer';
-import '../css/normalize.css';
 
 let rootElement = document.getElementById('container');
 
